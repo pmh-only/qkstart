462e939a0821ad74953312fa36e49703744cd398
Minhyeok Park <pmh_only@pmh.codes>
2024-07-18 10:37:35 +0000

    feat: add many install commands

--- scripts/install/alias.sh
+++ scripts/install/alias.sh
@@ -1,6 +1,6 @@
 cat <<EOF >> ~/.bashrc >> ~/.zshrc
 alias ka="kubectl apply -f"
-alias kd="kubectl delete -f"
-alias kde="kubectl describe -f"
+alias kd="kubectl describe -f"
+alias kx="kubectl delete -f"
 alias kg="kubectl get -f"
 EOF
